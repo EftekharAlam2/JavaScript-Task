405db063d1af4f3bb2d75e35f9a60de90ce25749
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-07-27 23:19:37 +0600

    task 01 done

--- Task.js
+++ Task.js
@@ -1,1 +1,12 @@
 // Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+
+function reverseString(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str.charAt(i);
+  }
+  return reversed;
+}
+const inputString = "hello, world";
+const reversedString = reverseString(inputString);
+console.log(reversedString);
